2da4c148d27cac3601d4b62f63f6414703110f4f
disp1air <hola-shola@mail.ru>
2018-08-31 16:06:43 +0300

    binary search and tests

---
+++ groc_algo/ch_1/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(list, item):
+  """
+  Функция binary_search получает отсортированный массив и значение.
+  Если значение присутствует в массиве, то функция возвращает его позицию.
+  """
+
+  low = 0
+  high = len(list) - 1
+
+  while high >= low:
+    mid = (low + high) // 2
+    guess = list[mid]
+    
+    if guess == item:
+      return mid
+    if guess > item:
+      high = mid - 1
+    else:
+      low = mid + 1
+
+  return None
