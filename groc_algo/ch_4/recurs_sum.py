e0723f2557bb0d61c37c5ca980e1edf277193491
disp1air <hola-shola@mail.ru>
2018-09-01 09:59:32 +0300

    about recursion

---
+++ groc_algo/ch_4/recurs_sum.py
@@ -0,0 +1,7 @@
+def recurs_sum(arr):
+  if len(arr) == 1:
+    return arr[0]
+  else:
+    return arr[0] + recurs_sum(arr[1:])
+
+print(recurs_sum([1, 2, 5, 22]))
---
+++ groc_algo/ch_4/sum.py
@@ -0,0 +1,8 @@
+def sum(arr):
+  res = 0
+  for x in arr:
+    res += x
+
+  return res
+
+print(sum([1, 2, 5]))
